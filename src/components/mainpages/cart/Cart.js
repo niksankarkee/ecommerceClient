fb99fe5902668bf53975da5bcdc7daca48c82316
niksankarkee <karkeeniksan@gmail.com>
2020-09-27 12:18:55 +0900

    increment/decrement cart item

--- src/components/mainpages/cart/Cart.js
+++ src/components/mainpages/cart/Cart.js
@@ -1,11 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { GlobalState } from "../../../GlobalState";
 import { Link } from "react-router-dom";
 
 export default function Cart() {
   const state = useContext(GlobalState);
-  const [cart] = state.userAPI.cart;
+  const [cart, setCart] = state.userAPI.cart;
   const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    const getTotal = () => {
+      const total = cart.reduce((prev, item) => {
+        return prev + item.price * item.quantity;
+      }, 0);
+      setTotal(total);
+    };
+    getTotal();
+  }, [cart]);
+
+  const increment = (id) => {
+    cart.forEach((item) => {
+      if (item._id === id) {
+        item.quantity += 1;
+      }
+    });
+    setCart([...cart]);
+  };
+  const decrement = (id) => {
+    cart.forEach((item) => {
+      if (item._id === id) {
+        item.quantity === 1 ? item.quantity = 1: item.quantity -= 1;
+      }
+    });
+    setCart([...cart]);
+  };
+
 
   if (cart.length === 0) {
     return (
@@ -15,7 +43,7 @@ export default function Cart() {
   return (
     <div>
       {cart.map((product) => (
-        <div className="detail cart">
+        <div className="detail cart" key={product._id}>
           <img src={product.images.url} alt="" />
           <div className="box-detail">
             <div className="row">
@@ -26,9 +54,9 @@ export default function Cart() {
             <p>{product.description}</p>
             <p>{product.content}</p>
             <div className="amount">
-              <button> - </button>
+              <button onClick={() => decrement(product._id)}> - </button>
               <span>{product.quantity}</span>
-              <button> + </button>
+              <button onClick={() => increment(product._id)}> + </button>
             </div>
             <div className="delete">X</div>
           </div>
